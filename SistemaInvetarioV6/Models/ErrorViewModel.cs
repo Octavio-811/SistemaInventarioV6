75ad9c8598caab49f737c8288c15cac5f58d57a6
David O Reynoso <octavio_montee_rubio@hotmail.com>
2023-06-30 14:11:17 -0600

    Crear Proyecto

---
+++ SistemaInvetarioV6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaInvetarioV6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
